6a79cdfb4dc24369f56b119f812850731b995344
Alain Plattner <alainplattner@gmail.com>
2018-12-18 10:57:52 -0600

    Added Langlais et al 2014 decorrelation depth estimation for nonzonal components

--- getdecodepth_Langlais.m
+++ getdecodepth_Langlais.m
@@ -1,7 +1,9 @@
-function [A,r,modval,modls]=getdecodepth(spec,ls,rplanet,rspec,showit)
-% [A,r,modval,modls]=getdecodepth(spec,ls,rplanet,rspec,showit)
+function [C,rc,modval,modls]=getdecodepth_Langlais(spec,ls,rplanet,showit)
+% [C,rc,modval,modls]=getdecodepth_Langlais(spec,ls,rplanet,showit)
 %
-% Calculates the decorrelation depth described by Voorhies et al (2002),
+% SPEC MUST ONLY CONTAIN NON-ZONAL COEFFICIENTS
+%
+% Calculates the decorrelation depth described by Langlais et al (2014),
 % eq. (5b) by doing a linear least squares fit to the logarithms.
 % 
 % INPUT:
@@ -10,47 +12,49 @@ function [A,r,modval,modls]=getdecodepth(spec,ls,rplanet,rspec,showit)
 % ls        spherical-harmonic degrees for which the spectral values are
 %           given. Must not contain 0!!! (log of zero is not a good thing)
 % rplanet   radius of the planet
-% rspec     radial position at which the spectrum is given
 % showit    would you like to see a plot showing the fit? 1 for yes,
 %           0 for no (default=0)
 %
 % OUTPUT:
 %
-% A         power factor (see Voorhies et al paper)
-% r         radial position of the random dipoles 
+% C         constant of flat spectrum
+% rc        radial position for a flat spectrum 
 %           (decorellation radial position)
 % modlval   spectrum from fitting
 % modls     degrees for fitted spectrum
 % 
-% Last modified by plattner-at-alumni.ethz.ch, 12/18/2018
+% Last modified by plattner-at-alumni.ethz.ch, 12/17/2018
   
 if ls(1)==0
    error('Can not have degree 0')
 end
 
-if nargin<5
-    showit=0;
+if nargin<4
+  showit=0;
 end
 
 spec=spec(:);
 ls=ls(:);
 
-y=log(spec./(ls.*(ls+0.5).*(ls+1)));
-x=ls;
+x=2*ls+4;
+y=-log(spec);
 
 [m,q]=fitlin(x,y);
+
+
 
 if showit
     plot(x,y,'x')
     hold on
     plot([x(1) x(end)],m*[x(1) x(end)]+q,'r--')
+    hold off
 end
 
-r=exp(0.5*(m-2*log(rplanet/rspec)))*rplanet;
-A=exp(q+2*log(r/rplanet)-4*log(rplanet/rspec));
+C=exp(-q);
+rc=rplanet/exp(m);
 
-modval=A*ls.*(ls+0.5).*(ls+1).*(r/rplanet).^(2*ls-2).*(rplanet/rspec).^(2*ls+4);
 modls=ls;
+modval=C*(rc/rplanet).^(2*ls+4);
 
 end
 
